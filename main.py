e62f6310826d2f3da36e3c98d81104c30989e0b3
jay3dec <jay3dec@gmail.com>
2022-01-16 23:35:26 +0530

    adding changes

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from typing import Optional
-
 from fastapi import FastAPI
+rootApp = FastAPI()
 
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@rootApp.get("/fetchEmployeeData")
+def fetchEmployeeData():
+    return {"employees": [{
+      "firstName" : "Roy"
+    },{
+      "firstName" : "Sam"
+    }]}
